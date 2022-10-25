9a0492e75c01cbd5f630736ead2eaac751b9b94b
Elijah-Igbokah <igbokah@gmail.com>
2022-10-25 08:46:53 +0100

    mobile responsiveness

--- src/components/Header-hero/Header-hero.component.jsx
+++ src/components/Header-hero/Header-hero.component.jsx
@@ -11,6 +11,12 @@ import outerIcon2 from "../../images/svgexport-3.svg";
 import outerIcon3 from "../../images/svgexport-20.svg";
 import middleIcon1 from "../../images/svgexport-5.svg";
 import middleIcon2 from "../../images/svgexport-6.svg";
+import license1 from "../../images/license1.svg";
+import license2 from "../../images/license2.svg";
+import playStore from "../../images/google-play-61cb4b62e2b04e3c1c5e6908b4cc7f6a.svg";
+import appStore from "../../images/app-store-9df3d746121bcf93fcdce6bff77758b5.svg";
+import kudaWeb from "../../images/kuda-web-77bc3dc525d8fc20bddde7f8195a4c17.svg"
+
 
 import "../button.components/get-kuda-btn.component";
 import GetKudaButton from "../button.components/get-kuda-btn.component";
@@ -22,6 +28,16 @@ const Hero = () =>{
                 <h1>We’re the bank of the free</h1>
                 <h3>We’re here to help you get the best out of your money, no strings attached.</h3>
                 <h3>Welcome to your freedom!</h3>
+                <div className="store-icons">
+                    <img src={appStore} alt="" />
+                    <img src={playStore} alt="" />
+                    <img src={kudaWeb} alt="" />
+                </div>
+                <div className="license">
+                    <img src={license1} alt="" />
+                    <img src={license2} alt="" />
+                </div>
+                
                 <div className="get-kuda-btn">
                     <GetKudaButton/>
                 </div>
